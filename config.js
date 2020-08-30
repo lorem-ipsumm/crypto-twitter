99d0a64b3728baaca4e071890a0e6522c3947073
nick <morri2ne@dukes.jmu.edu>
2020-08-30 12:32:46 -0400

    fixed?

--- config.js
+++
@@ -1,8 +0,0 @@
-let config = {};
-
-config.consumer_key = "3hWbGGSzreRtBCKhUNi4yweiz",
-config.consumer_secret = "GKSOqI1M57uqaMi67ptPpGGVKegPAJusjg2FcLLHMynR5E9mF5",
-config.access_token_key = "818642221645832193-PfCL2WUwdnnvReBCnre110i2XoCWqJa",
-config.access_token_secret = "p3TX96rr3ZfzH25DAh459iEEVvrx0YnSE3XQ83QDTRYda"
-
-module.exports = config;
