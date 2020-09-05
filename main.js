ab6894f7a8126c8b56ea633680de0a410f8a9e8b
nick <morri2ne@dukes.jmu.edu>
2020-09-05 19:17:13 -0400

    added console timestamps

--- main.js
+++ main.js
@@ -41,8 +41,12 @@ var cheerio = require("cheerio");
 var fs = require("fs");
 var Twitter = require("twitter-lite");
 // provide your own config with keys
+// comment this section out if you don't have keys and
+// and you just want to scrape Coingecko
 var Config = require("./config");
 // setup client
+// comment this section out if you don't have keys and
+// and you just want to scrape Coingecko
 var client = new Twitter({
     consumer_key: Config.consumer_key,
     consumer_secret: Config.consumer_secret,
@@ -62,19 +66,21 @@ function newTweet(coinData) {
                         "\n1h Change: " + coinData.hour +
                         "\n24h Change: " + coinData.day +
                         "\n24h Volume: " + coinData.volume +
-                        "\nMkt Cap: " + coinData.marketCap +
                         "\n\n" + coinData.url;
+                    // post the tweet
                     return [4 /*yield*/, client.post("statuses/update", {
                             status: tweet
                         })
                             .then(function () {
-                            console.log("tweet sent");
+                            console.log(new Date().toJSON());
+                            console.log("tweet sent: \n" + tweet);
                         })["catch"](function (err) {
+                            console.log(new Date().toJSON());
                             console.log(err);
                         })];
                 case 1:
+                    // post the tweet
                     _a.sent();
-                    console.log(tweet);
                     return [2 /*return*/];
             }
         });
@@ -83,64 +89,74 @@ function newTweet(coinData) {
 // scrape the recently added page
 function scrape() {
     return __awaiter(this, void 0, void 0, function () {
-        var html, $;
+        var coins, html, $;
+        var _this = this;
         return __generator(this, function (_a) {
             switch (_a.label) {
-                case 0: return [4 /*yield*/, axios_1["default"].get("https://www.coingecko.com/en/coins/recently_added")];
+                case 0:
+                    console.log(new Date().toJSON());
+                    console.log("scraping");
+                    coins = "";
+                    try {
+                        coins = fs.readFileSync("coins.txt", "utf8");
+                    }
+                    catch (err) {
+                        console.log(err);
+                        return [2 /*return*/];
+                    }
+                    return [4 /*yield*/, axios_1["default"].get("https://www.coingecko.com/en/coins/recently_added")];
                 case 1:
                     html = _a.sent();
                     return [4 /*yield*/, cheerio.load(html.data)];
                 case 2:
                     $ = _a.sent();
                     // go through all table items
-                    $('tr', 'tbody').each(function (i, elem) {
-                        var coinName;
-                        var coinTicker;
-                        // get token info
-                        var nameInfo = $(elem).find(".coin-name a").text().trim().split("\n").join().split(",,");
-                        coinName = nameInfo[0];
-                        coinTicker = nameInfo[1];
-                        coinTicker = coinTicker.trim();
-                        // get data
-                        var price = $(elem).find(".td-price span").text().trim();
-                        var hourChange = $(elem).find(".td-change1h span").text().trim();
-                        var dayChange = $(elem).find(".td-change24h span").text().trim();
-                        var volume = $(elem).find(".td-liquidity_score span").text().trim();
-                        var marketCap = $(elem).find(".td-market_cap").text().trim();
-                        var url = "https://www.coingecko.com" + $(elem).find(".coin-name a").attr("href");
-                        // check if price change data has been added
-                        if (hourChange.length <= 1)
-                            hourChange = "N/A";
-                        if (dayChange.length <= 1)
-                            dayChange = "N/A";
-                        var coinData = {
-                            name: coinName,
-                            ticker: coinTicker,
-                            price: price,
-                            hour: hourChange,
-                            day: dayChange,
-                            volume: volume,
-                            marketCap: marketCap,
-                            url: url
-                        };
-                        // check our saved file
-                        // TODO: no need to read the file in the loop
-                        fs.readFile("coins.txt", function (err, data) {
-                            if (err) {
-                                console.log(err);
-                                return;
-                            }
-                            // check if coin has already been added
-                            if (data.indexOf(coinName + "(" + coinTicker + ")") === -1) {
-                                // append coin name to text file
-                                fs.appendFile("coins.txt", "\n" + coinName + "(" + coinTicker + ")", function (err) {
-                                    if (err)
-                                        console.log(err);
-                                });
-                                newTweet(coinData);
+                    $('tr', 'tbody').each(function (i, elem) { return __awaiter(_this, void 0, void 0, function () {
+                        var coinName, coinTicker, nameInfo, price, hourChange, dayChange, volume, marketCap, url, coinData;
+                        return __generator(this, function (_a) {
+                            switch (_a.label) {
+                                case 0:
+                                    nameInfo = $(elem).find(".coin-name a").text().trim().split("\n").join().split(",,");
+                                    coinName = nameInfo[0];
+                                    coinTicker = nameInfo[1];
+                                    coinTicker = coinTicker.trim();
+                                    price = $(elem).find(".td-price span").text().trim();
+                                    hourChange = $(elem).find(".td-change1h span").text().trim();
+                                    dayChange = $(elem).find(".td-change24h span").text().trim();
+                                    volume = $(elem).find(".td-liquidity_score span").text().trim();
+                                    marketCap = $(elem).find(".td-market_cap").text().trim();
+                                    url = "https://www.coingecko.com" + $(elem).find(".coin-name a").attr("href");
+                                    // check if price change data has been added
+                                    if (hourChange.length <= 1)
+                                        hourChange = "?";
+                                    if (dayChange.length <= 1)
+                                        dayChange = "?";
+                                    coinData = {
+                                        name: coinName,
+                                        ticker: coinTicker,
+                                        price: price,
+                                        hour: hourChange,
+                                        day: dayChange,
+                                        volume: volume,
+                                        marketCap: marketCap,
+                                        url: url
+                                    };
+                                    if (!(coins.indexOf(coinName + "(" + coinTicker + ")") === -1)) return [3 /*break*/, 2];
+                                    console.log(new Date().toJSON());
+                                    console.log("New coin found: " + coinName + " / $" + coinTicker);
+                                    // append coin name to text file
+                                    return [4 /*yield*/, fs.appendFile("coins.txt", "\n" + coinName + "(" + coinTicker + ")", function (err) {
+                                            if (err)
+                                                console.log(err);
+                                        })];
+                                case 1:
+                                    // append coin name to text file
+                                    _a.sent();
+                                    _a.label = 2;
+                                case 2: return [2 /*return*/];
                             }
                         });
-                    });
+                    }); });
                     return [2 /*return*/];
             }
         });
@@ -149,4 +165,6 @@ function scrape() {
 console.log("starting!");
 // scrape every 10 minutes
 scrape();
-setInterval(function () { scrape(); }, 600000);
+setInterval(function () {
+    scrape();
+}, 600000);
